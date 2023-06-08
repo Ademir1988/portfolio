11d3ad5240eb0ac78f4ed5b15b53c39fdcd363e8
Ademirsilva <87206431+Ademir1988@users.noreply.github.com>
2023-06-08 18:23:55 -0300

    mexendo no css

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -10,13 +10,14 @@ const Navbar = () => {
         <h3 className={styles.icon}><HiOutlineCode/></h3>
         
         <div className={styles.content}>
-        <Link to='/' className={styles.link}>Home</Link>
-        <Link to='/Sobre' className={styles.link}>Sobre</Link>
-        <Link to='/Tecnologias' className={styles.link}>Tecnologias</Link>
-        <Link to='/Projetos' className={styles.link}>Projetos</Link>
-        <Link to='/Contact' className={styles.link}>Contatos</Link>
+
+          <Link to='/' className={styles.link}>Home</Link>
+          <Link to='/Sobre' className={styles.link}>Sobre</Link>
+          <Link to='/Tecnologias' className={styles.link}>Tecnologias</Link>
+          <Link to='/Projetos' className={styles.link}>Projetos</Link>
+          <Link to='/Contact' className={styles.link}>Contatos</Link>
         
-      </div>
+         </div>
         
        
       </nav>
