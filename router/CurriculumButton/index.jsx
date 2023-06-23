962e51de41edbef651d4676df6198edbccca15a0
Ademirsilva <87206431+Ademir1988@users.noreply.github.com>
2023-06-23 16:56:57 -0300

    colocando botão estilizado

--- router/CurriculumButton/index.jsx
+++ router/CurriculumButton/index.jsx
@@ -1,15 +1,24 @@
 import '../CurriculumButton/index.css'
 import { SiReact } from 'react-icons/Si';
 
-export default function CurriculumButton(){
-    return(
+export default function CurriculumButton() {
+    return (
         <article>
-            <h2><SiReact/></h2>     
-        <button className='btn_curriculo'>
+            <h2><SiReact /></h2>
+
+            <button class="learn-more">
+                <span class="circle" aria-hidden="true">
+                    <span class="icon arrow"></span>
+                </span>                
+                <span class="button-text">
+                    <a href="https://ademir1988.github.io/curriculo/" target='_black'>Currículo</a>
+                    </span>
+            </button>
+            {/* <button className='btn_curriculo'>
             <a href=" https://ademir1988.github.io/curriculo/" target='_black'>Currículo</a>  
                     
-        </button>
-            
-      </article>
+        </button> */}
+
+        </article>
     )
 }
