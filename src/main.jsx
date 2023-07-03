cd148885cdd5de08e190349c737cef7aeda58aa0
Ademirsilva <87206431+Ademir1988@users.noreply.github.com>
2023-07-03 12:30:12 -0300

    k

--- src/main.jsx
+++ src/main.jsx
@@ -13,7 +13,7 @@ import Sobre from '../router/Sobre'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-  <BrowserRouter basename='/'>  
+  <HashRouter basename='/'>  
   <Routes>
   <Route path="/" element={<App />}>
   <Route index element={<Home />} />
@@ -22,6 +22,6 @@ ReactDOM.createRoot(document.getElementById('root')).render(
   <Route path="/projetos" element={<Projetos />} />
   </Route>
   </Routes>  
-  </BrowserRouter>
+  </HashRouter>
   </React.StrictMode>
   );
